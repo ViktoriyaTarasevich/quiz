bf628fc9115b964d7831006e591cf68888a4d485
ViktoriyaTarasevich <viktoriya.v.tarasevich@gmail.com>
2014-07-15 16:42:39 +0300

    [Update]: fix one implement.

--- javascript1/index.js
+++ javascript1/index.js
@@ -3,44 +3,41 @@ var b = 0;
 
 var flag = false;
 //1 
-var c = (flag*a + (1-flag)*b);
+var c = (flag * a + (1 - flag) * b);
 //2
 var c = (flag && a || b);
 
 //3
-function getValueFromException (){
+function getValueFromException() {
 
-	function throwIfFalse()
-	{
-		throw 'false';
-	}
-	try{
-		var temp = !flag && throwIfFalse();
-		
-			return a;
-	}
-	catch(e){
-		
-			return b;
-	}
+    function throwIfFalse() {
+        throw 'false';
+    }
+    try {
+        var temp = !flag && throwIfFalse();
+        return a;
+    }
+    catch (e) {
+        return b;
+    }
 }
 
 //4
 
-function getValueFromArray(){
-	var list = [a,b];
-	var temp = Number(flag);
-	return list[temp];
+function getValueFromArray() {
+    var list = [a, b];
+    var temp = Number(flag);
+    return list[temp];
 }
 
 //5
-var c = eval(a*flag || b*(!flag));
+var c = eval(a * flag || b * (!flag));
 
 //6
-function getValueFromRegexp(){
-	var str = flag.toString();
-	var res = (str.match(/true/) || a) && (str.match(/false/) || b);
-	return res;
+function getValueFromRegexp() {
+    var str = flag.toString();
+    var res = (str.match(/true/) || a) ^ (str.match(/false/) || b);
+    return res;
 }
 
 //7
@@ -49,10 +46,10 @@ var c = (flag || a) ^ (flag || b);
 
 //8
 
-function getValueFromArrayTwo(){
-	var tempList = [0,1];
-	var tempNumper = Number(flag) + 1;
-	var c =  (((tempNumper) in tempList || a ) ^ ((tempNumper) in tempList || b));
-	return c;
+function getValueFromArrayTwo() {
+    var tempList = [0, 1];
+    var tempNumper = Number(flag) + 1;
+    var c = (((tempNumper) in tempList || a ) ^ ((tempNumper) in tempList || b));
+    return c;
 }
 
