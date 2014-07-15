7f0121c747c4c9bab360008dfa098156988cfc1e
ViktoriyaTarasevich <viktoriya.v.tarasevich@gmail.com>
2014-07-15 07:50:17 +0300

    [Update]: added three implements.

--- javascript1/index.js
+++ javascript1/index.js
@@ -2,9 +2,38 @@ var a = 3;
 var b = 5;
 
 var flag = false;
-
-var c = (flag*a + (1-flag)*b); //3
-flag = true; 
-var c = (flag*a + (1-flag)*b); //5
-
+//1 
+var c = (flag*a + (1-flag)*b);
+//2
 var c = (flag && a || b);
+
+//3
+function getValueFromException (){
+
+	function throwIfFalse()
+	{
+		throw 'false';
+	}
+	try{
+		var temp = !flag && throwIfFalse();
+		
+			return a;
+	}
+	catch(e){
+		
+			return b;
+	}
+}
+
+//4
+
+function getValueFromArray(){
+	var list = [a,b];
+	var temp = Number(flag);
+	return list[temp];
+}
+
+//5
+var c = eval(a*flag || b*(!flag));
+
+
